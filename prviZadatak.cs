cc55c051df1f87763cde65bab0d8f73014648c08
XDomeksX <90246598+XDomeksX@users.noreply.github.com>
2021-09-21 11:19:54 +0200

    Create prviZadatak.cs

---
+++ prviZadatak.cs
@@ -0,0 +1,32 @@
+/*
+  Domenik Milohanić
+  3.d
+  21.9.2021.
+  hello world
+  
+  Varijable u C#
+  Napišite program u kojem ćete deklarirati varijable odgovarajućeg tipa, te im dodijelite sljedeće vrijednosti:
+  100
+  -1000
+  a
+  3.123
+  3.123456789
+  true
+  "true"
+*/
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int prvaVarijabla = 100;
+		int drugaVarijabla = -1000;
+		char trecaVarijabla = 'a';
+		float cetvrtaVarijabla = 3.123F;
+		float petaVarijabla = 3.123456789F;
+		bool sestaVarijabla = true;
+		string sedmaVarijabla = "true";
+	}
+}
